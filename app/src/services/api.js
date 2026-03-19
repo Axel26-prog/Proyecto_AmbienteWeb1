ff9ee06547017e3678364cae60ba1b5d9b2fe680
Axel26-prog <axelguzman996@gmail.com>
2026-03-19 11:16:56 -0600

    Final merge

--- app/src/services/api.js
+++ app/src/services/api.js
@@ -2,24 +2,6 @@ const API_BASE =
   import.meta.env.VITE_API_URL || "http://localhost:81/appsubasta/api";
 
 export async function apiFetch(endpoint, options = {}) {
-<<<<<<< HEAD
-  const response = await fetch(`${API_BASE}/${endpoint}`, options);
-
-  if (!response.ok) {
-    throw new Error("Error en la petición");
-  }
-
-  let json = null;
-
-  try {
-    json = await response.json();
-  } catch (error) {
-
-    return null;
-  }
-
-  return json?.data ?? json;
-=======
   const response = await fetch(`${API_BASE}/${endpoint}`, {
     method: options.method || "GET",
     headers: {
@@ -37,5 +19,4 @@ export async function apiFetch(endpoint, options = {}) {
   }
 
   return json.data ?? json;
->>>>>>> Mant_Usuarios
 }
